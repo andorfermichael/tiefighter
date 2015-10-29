d7d581d6c865711fe69e787a891c9286d3cff1ab
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2015-10-29 10:11:46 +0100

    Initial Commit

---
+++ Tie Fighter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tie Fighter
+//
+//  Created by Michael Andorfer on 29.10.15.
+//  Copyright © 2015 FH Salzburg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
